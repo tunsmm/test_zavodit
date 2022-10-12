73438d8dea56e1982889ed97ba04f58133eb49ab
tunsmm <dragon553180@gmail.com>
2022-10-13 01:10:13 +0300

    Delete extra

--- mass_media/api/urls.py
+++ mass_media/api/urls.py
@@ -1,16 +1,12 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.routers import SimpleRouter
 
 from .views import NewsViewSet
 
 
 router = SimpleRouter()
-# router = DefaultRouter()
 
 router.register(r'news', NewsViewSet)
-# router.register('news', NewsViewSet, basename='news')
-# router.register('news/<int:pk>', NewsViewSet.as_view({'delete': 'destroy'}), basename='news-change')
-
 
 urlpatterns = [
     path('v1/', include(router.urls)),
